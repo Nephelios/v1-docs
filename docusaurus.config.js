5b2ba4f1224c4662dbd37568c160e1f649f8c6cd
ZUHOWKS <jojo.vilard38@gmail.com>
2025-04-16 01:52:45 +0200

    Nephelios config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://exemple.com",
+  url: "https://nephelios.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/v1-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
